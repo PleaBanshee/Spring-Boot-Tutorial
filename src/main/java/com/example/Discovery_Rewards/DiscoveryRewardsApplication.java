f7934274c0d301f30b6da1a492beeae62bb99ff0
PleaBanshee <llewellynant@gmail.com>
2021-08-28 08:47:25 +0200

    Added Project Files

---
+++ src/main/java/com/example/Discovery_Rewards/DiscoveryRewardsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Discovery_Rewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryRewardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryRewardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Discovery_Rewards/DiscoveryRewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Discovery_Rewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryRewardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
